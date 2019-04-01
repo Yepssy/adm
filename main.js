e127c9db21f4370b69b42a345f548578657f3fda
Yepssy <jessy.fallavier@lilo.org>
2019-04-01 23:00:41 +0200

    f

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ document.onmousedown=BoutonDroit;
 document.oncontextmenu =Alerte;
 document.onselectstart=Alerte;
 
+
+function myFunction() {
+  var img = document.getElementById("up");
+  var t = img.style.transform.match(/(\d+)/g) || [0];  // on met ||[0) pour le 1st passage
+  var val = ( t[0] *1 +40) % 360;                      // incrémentation de 40
+  img.style.transform = 'rotate(' +val +'deg)';
+}
+
 Xm = 3 ;
 X = 0 ;
 let n = 25;
