658429d4542ecb217fe8ed9ff1f4264e39b33363
Yepssy <jessy.fallavier@lilo.org>
2019-04-02 22:01:30 +0200

    nul

--- main.js
+++ main.js
@@ -3,28 +3,44 @@ function alerte(){
   return false
 }
 
-function rotateCmd() {
+function rotateCmd(sens) {
   var img = document.getElementById("rotate");
   var t = img.style.transform.match(/(\d+)/g) || [0];
-  val = ( t[0] *1 +1) % 360;
+  var val = ( t[0] *1 +1) % 360;      
   img.style.transform = 'rotate(' +val +'deg)'
+  
+/*
+    if (sens == 1) {
+    var val = ( t[0] *1 +1) % 360;      
+    img.style.transform = 'rotate(' +val +'deg)'
+    
+  } else {
+    var val = ((( t[0] *1 + 1) % 360 ) - 360) * -1;
+    img.style.transform = 'rotate(' +val +'deg)'
+    
+  }
+*/
 
   if (val == 358) {
+    alert("no")
     clearInterval(interv);
     delete(interv);
     val = 0;
+    
   }
 }
 
 function rotate() 
 {
-  if (typeof interv == "undefined") {
-    if (sens == "undefined") {
-      
+  if (typeof(interv) == "undefined") {
+/*    
+    if (sens == -1) {
+      var sens = 1;
     } else {
-      
+      var sens = -1;
     }
-    interv = setInterval(rotateCmd, 5)
+*/
+    interv = setInterval(rotateCmd(sens), 5)
   }
 }
 
@@ -41,10 +57,10 @@ document.images[X].style.display = "initial";
 setTimeout(next, n) ;
 }
 
-//document.onmousedown=alerte; 
-//document.oncontextmenu =alerte;
-//document.onselectstart=alerte;
-
+document.onmousedown=alerte; 
+document.oncontextmenu =alerte;
+document.onselectstart=alerte;
+var sens = false;
 Xm = 3 ;
 X = 0 ;
 var n = 25;
