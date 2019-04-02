dcdb71487e766ec2b6e47366bc48d209980f1098
Yepssy <jessy.fallavier@lilo.org>
2019-04-02 22:01:34 +0200

    Revert "nul"
    This reverts commit 658429d4542ecb217fe8ed9ff1f4264e39b33363.

--- main.js
+++ main.js
@@ -3,44 +3,28 @@ function alerte(){
   return false
 }
 
-function rotateCmd(sens) {
+function rotateCmd() {
   var img = document.getElementById("rotate");
   var t = img.style.transform.match(/(\d+)/g) || [0];
-  var val = ( t[0] *1 +1) % 360;      
+  val = ( t[0] *1 +1) % 360;
   img.style.transform = 'rotate(' +val +'deg)'
-  
-/*
-    if (sens == 1) {
-    var val = ( t[0] *1 +1) % 360;      
-    img.style.transform = 'rotate(' +val +'deg)'
-    
-  } else {
-    var val = ((( t[0] *1 + 1) % 360 ) - 360) * -1;
-    img.style.transform = 'rotate(' +val +'deg)'
-    
-  }
-*/
 
   if (val == 358) {
-    alert("no")
     clearInterval(interv);
     delete(interv);
     val = 0;
-    
   }
 }
 
 function rotate() 
 {
-  if (typeof(interv) == "undefined") {
-/*    
-    if (sens == -1) {
-      var sens = 1;
+  if (typeof interv == "undefined") {
+    if (sens == "undefined") {
+      
     } else {
-      var sens = -1;
+      
     }
-*/
-    interv = setInterval(rotateCmd(sens), 5)
+    interv = setInterval(rotateCmd, 5)
   }
 }
 
@@ -57,10 +41,10 @@ document.images[X].style.display = "initial";
 setTimeout(next, n) ;
 }
 
-document.onmousedown=alerte; 
-document.oncontextmenu =alerte;
-document.onselectstart=alerte;
-var sens = false;
+//document.onmousedown=alerte; 
+//document.oncontextmenu =alerte;
+//document.onselectstart=alerte;
+
 Xm = 3 ;
 X = 0 ;
 var n = 25;
